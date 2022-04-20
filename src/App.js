79713c9e30c33a3804220b3bf59546de435e9be2
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-20 20:36:30 +0200

    add req

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+
+
+const fetchDoggo = async () =>{
+  const req = await axios.get('https://dog.ceo/api/breeds/image/random')
+
+  return req.data.message
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random doggo</h1>
+    {}
     </div>
   );
 }
