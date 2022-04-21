27f9c553afceae8b173d8d61c6d73a1d92810f02
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-21 20:14:08 +0200

    added dropdown list

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { useEffect, useState } from 'react';
 
 
 
-const fetchDoggo = async  () =>{
+const fetchDoggo = async  (breed) => {
+  if(breed){
+   const fetch = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random`)
+   return fetch
+  }
   const fetch = await axios.get('https://dog.ceo/api/breeds/image/random')
-  console.log(fetch)
   return fetch
 }
 
@@ -21,6 +24,8 @@ const fetchBreeds = async  () =>{
 function App() {
   const [doggo, setDoggo] = useState('')
   const [breeds, setBreeds] = useState([])
+  // const [selectedBreed,setSelectedBreed]= useState('')
+
   useEffect(() => {
     fetchDoggo().then((response) => setDoggo(response.data.message))
   
@@ -54,10 +59,11 @@ const makeBreedsList = (breeds) =>{
       <h1>Random doggo</h1>
       <img src={doggo} alt="doggo"></img>
       <div className='button-wrapper'>
-       <button className='button' onClick={()=>fetchDoggo().then((response) => setDoggo(response.data.message))}>New doggo</button>
+       <button className='button' onClick={()=>fetchDoggo('husky').then((response) => setDoggo(response.data.message))}>New doggo</button>
        </div>
+       {/* <select onChange={() => setSelectedBreed(e.target.options[e.target.selectedIndex])}> */}
        <select>
-       
+       <option> all </option>
        {breedArr.map((item,i) => <option key={i}>{item}</option>)}
        
      </select>
