d3463b922b903e1ddc0d9a394c8ae88569cbaa79
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-21 15:27:51 +0200

    implemented display image and new doggo button

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import { useEffect, useState } from 'react';
 
 
 
-const fetchDoggo = async () =>{
-  const req = await axios.get('https://dog.ceo/api/breeds/image/random')
-
-  return req.data.message
+const fetchDoggo = async  () =>{
+  const fetch = await axios.get('https://dog.ceo/api/breeds/image/random')
+  console.log(fetch)
+  return fetch
 }
 
-function App() {
+// const fetchBreeds = async  () =>{
+//   const fetch = await axios.get('https://dog.ceo/api/breeds/list/all')
+//   console.log(fetch)
+//   return JSON.stringify(fetch.data.message)
+// }
 
+// 
+function App() {
+  const [doggo, setDoggo] = useState('')
+
+  useEffect(() => {
+    fetchDoggo().then((response) => setDoggo(response.data.message))
+  
+}, [])
   return (
     <div className="App">
       <h1>Random doggo</h1>
-    {}
+      <img src={doggo} alt="doggo"></img>
+      <div className='button-wrapper'>
+       <button className='button' onClick={()=>fetchDoggo().then((response) => setDoggo(response.data.message))}>New doggo</button>
+       </div>
     </div>
   );
 }
